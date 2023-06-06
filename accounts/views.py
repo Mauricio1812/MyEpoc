eaa51c45e05ebbfe2117895c05851552b4673427
Mauricio1812 <mauriciodariosalazarespinosa@gmail.com>
2023-06-05 22:47:36 -0500

    Fixed user signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.conf import settings
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 
 from . import forms
 
@@ -11,7 +11,15 @@ def signup_page(request):
         form = forms.SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # auto-login user
+            user.refresh_from_db()
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, 'registration/signup.html', context={'form': form})
+
