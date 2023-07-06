e71fc221a3c8506a5aa2ed66b66dde6632d06a7d
Mauricio1812 <mauriciodariosalazarespinosa@gmail.com>
2023-07-06 14:27:09 -0500

    Removed unused templates

--- MyEpoc/urls.py
+++ MyEpoc/urls.py
@@ -28,6 +28,4 @@ urlpatterns = [
     path('signup/', include('accounts.urls'), name='signup'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
-    path("", index, name="home"),
-    #path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
--- epoc_app/urls.py
+++ epoc_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('snippets/',Temp_serializer_agregar_data,name ="Listado"),
     path('table',views.TableView.as_view(),name='table'),
     path('command_data',command_serializer, name='Command_list'),
-    re_path(r'^temp-chart/(?P<patient_id>[0-9]+)/$', views.temp_chart, name='temp-chart'),
-    re_path(r'^(?P<patient_id>[0-9]+)/$', views.details_pat, name='detail'),
-    #path('index/',index,name='home')
+    path('temp-chart/<int:patient_id>/', views.temp_chart, name='temp-chart'),
+    path('<int:patient_id>/', views.details_pat, name='detail'),
 ]
